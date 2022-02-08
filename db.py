aba270b34105aa5c2b9f9dbac83019b9769d8943
ivymmurithi <ivymurithi@gmail.com>
2022-02-08 09:33:23 +0300

    heroku postgres

--- db.py
+++ db.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import DevConfig, Config
+from config import DevConfig, Config, ProdConfig
 
 app = Flask(__name__)
-app.config.from_object(DevConfig)
+app.config.from_object(ProdConfig)
 app.config['SECRET_KEY']=Config.SECRET_KEY
 db = SQLAlchemy(app)
